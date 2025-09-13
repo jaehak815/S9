0223a09e296cdbbc604f85b0b3fd25192e79c8e1
Jay <rright815@gmail.com>
2025-09-13 22:21:25 +0800

    187/188

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { AppComponent } from './app/app.component';
 import { TasksService } from './app/tasks/tasks.service';
 import { InjectionToken } from '@angular/core';
 
-export const TasksServiceToken = new InjectionToken<TasksService>('tasks-service-token');
+export const TasksServiceToken = new InjectionToken<TasksService>(
+  'tasks-service-token'
+);
 
 bootstrapApplication(AppComponent, {
-    providers:[{provide: TasksServiceToken, useClass:TasksService}]
+  providers: [{ provide: TasksServiceToken, useClass: TasksService }],
 }).catch((err) => console.error(err));
 //bootstrapApplication(AppComponent).catch((err) => console.error(err));
