09cfa2d4963e8307576c427e06ec96dca7335e10
Jay <rright815@gmail.com>
2025-09-13 19:00:12 +0800

    177

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -11,14 +11,11 @@ import { TasksService } from '../tasks.service';
 })
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
-  private tasksService: TasksService;
 
-  constructor(){
-    this.tasksService = new TasksService();
-  }
+  constructor(private tasksService: TasksService) {}
 
   onAddTask(title: string, description: string) {
-    this.tasksService.addTask({title, description})
+    this.tasksService.addTask({ title, description });
     this.formEl()?.nativeElement.reset();
   }
 }
